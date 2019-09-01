c7e4447d340edaf91a047435483f9dc78020e5eb
Titus Ouko <titusouko@gmail.com>
2019-09-01 19:26:40 +0300

    Add conditional statement to check if oder is to be delivered or not to script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -44,12 +44,22 @@ $(document).ready(function () {
 
 
         var DeliveryCost = deliverPrices[pizzaPick - 1];
-        alert(DeliveryCost);
+
+
 
         newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
         newTotal = new Total(price, pizzaQty, DeliveryCost);
+        if (pizzaPick===1){
         alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
         alert("your bill is: " + newTotal.finalTotal());
+        }else{
+            if(pizzaPick===2){
+                prompt("Enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
+                alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal());
+            }
+        }
 
     });
 
